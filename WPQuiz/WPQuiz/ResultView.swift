c3e507aaed4641175f3512228a18d0e22819abf6
Mike Tubis <mike.tubis@gmail.com>
2017-03-10 18:03:38 +0100

    add QuizView.xib

---
+++ WPQuiz/WPQuiz/ResultView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultView.swift
+//  WPQuiz
+//
+//  Created by Michał Tubis on 10.03.2017.
+//  Copyright © 2017 Mike Tubis. All rights reserved.
+//
+
+import UIKit
+
+class ResultView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
